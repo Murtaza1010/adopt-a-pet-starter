0a294fb76477d595166d5cf0eaee4bea35f1c198
MK <mortezakarim0@gmail.com>
2022-11-13 22:09:53 +0000

    reactRouterPracticeExercise

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path='/:type/:id'><PetDetailsPage /></Route>
+          <Route path='/:type?'><HomePage /></Route>
+          <Route path='/pet-details-not-found'><PetDetailsNotFound /></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
